bbfec58022e72f6d9813adeec0aa37dd609b2f6c
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-12-29 15:53:43 +0530

    Init gh-pages

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const wait = require('gulp-wait');
+const babel = require('gulp-babel');;
+const rename = require('gulp-rename');
+var deploy = require('gulp-gh-pages');
+
+var browserSync = require('browser-sync').create();
+
+
+
+gulp.task('deploy', function () {
+    return gulp.src("./dist/**/*")
+      .pipe(deploy())
+  });
+
+gulp.task('scripts', function() {
+    return gulp.src('./js/scripts.js')
+        .pipe(plumber(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        })))
+        .pipe(babel({
+          presets: [['@babel/env', {modules:false}]]
+        }))
+        .pipe(uglify({
+            output: {
+                comments: '/^!/'
+            }
+        }))
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest('./js'));
+});
+
+gulp.task('styles', function () {
+    return gulp.src('./scss/styless.scss')
+        .pipe(wait(250))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./js/scripts.js', gulp.series('scripts'));
+    gulp.watch('./scss/styles.scss', gulp.series('styles'));
+});
+
+gulp.watch(['./**/*.{scss,css,html,py,js}'], ['clearCache', browserSync.reload]);
+
+gulp.task('clearCache', function (done) {
+   return cache.clearAll(done);
+});
